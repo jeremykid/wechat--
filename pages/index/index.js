5528aeb9d6d885696c0890f7cde15fb2cdf6a90b
weijiesun <weijie2@ualberta.ca>
2017-01-12 19:07:19 -0700

    Add the wp request

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,11 +3,13 @@
 var app = getApp()
 //get file component
 const AV = require('../../utils/av-weapp.js')
+var Util = require( '../../utils/util.js' );
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
-    tempFilePaths: '',
+    image1Path: '',
+    image2Path: '',
     title: '颜值打分神器',
     score: 90.99,
   },
@@ -27,11 +29,12 @@ Page({
       console.log(userInfo);
       that.setData({
         userInfo:userInfo,
-        tempFilePaths: userInfo.avatarUrl
+        image1Path: userInfo.avatarUrl,
+        image2Path: userInfo.avatarUrl,
       })
     })
   },
-  chooseimage: function () {
+  chooseimage1: function () {
     var _this = this;  
     wx.chooseImage({
       count: 9, // 默认9  
@@ -41,9 +44,26 @@ Page({
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
         console.log(res);
         _this.setData({  
-          tempFilePaths: res.tempFilePaths  
+          image1Path: res.tempFilePaths  
         })  
-       var tempFilePath = res.tempFilePaths[0];  
+       var image1Path = res.tempFilePaths[0];  
+
+      }  
+    })
+  },
+  chooseimage2: function () {
+    var _this = this;  
+    wx.chooseImage({
+      count: 9, // 默认9  
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有  
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有  
+      success: function (res) {  
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片  
+        console.log(res);
+        _this.setData({  
+          image2Path: res.tempFilePaths  
+        })  
+       var image2Path = res.tempFilePaths[0];  
 
       }  
     })
@@ -52,32 +72,50 @@ Page({
     this.setData({
         score: 80
       });
-    var result = this.data.tempFilePaths;
-    var image = new Image();
-    image.src = result;
+    var result = this.data.image1Path;
+    var image1 = new Image();
+    image1.src = result;
+
     // todo change 
-    // wx.request( {  
-    //   url: "http://op.juhe.cn/onebox/weather/query",  
-    //   header: {  
-    //     "Content-Type": "application/x-www-form-urlencoded"  
-    //   },  
-    //   method: "POST",  
-    //  //data: { cityname: "上海", key: "1430ec127e097e1113259c5e1be1ba70" },  
-    //   data: Util.json2Form( { cityname: "上海", key: "1430ec127e097e1113259c5e1be1ba70" }),  
-    //   complete: function( res ) {  
-    //     this.setData( {  
-    //       toastHidden: false,  
-    //       toastText: res.data.reason,  
-    //       city_name: res.data.result.data.realtime.city_name,  
-    //       date: res.data.result.data.realtime.date,  
-    //       info: res.data.result.data.realtime.weather.info,  
-    //     });  
-    //     if( res == null || res.data == null ) {  
-    //       console.error( '网络请求失败' );  
-    //       return;  
-    //     }  
-    //   }  
-    // });
+    wx.request( {  
+      url: "http://apis.baidu.com/idl_baidu/faceverifyservice/face_compare",
+      header: {  
+        "Content-Type": "application/x-www-form-urlencoded",
+        'apikey':'',  
+      },  
+      method: "POST",  
+     //data: { cityname: "上海", key: "1430ec127e097e1113259c5e1be1ba70" },  
+      data: ( {
+  "params": [
+    {
+      "cmdid": "1000",
+      "appid": "10000",
+      "clientip": "10.23.34.5",
+      "type": "st_groupverify",
+      "groupid": "0123456",
+      "versionnum": "1.0.0.1",
+      "usernames": {
+        "name2": "name2",
+        "name1": "name1"
+      },
+      "images": {
+        "name2": "/9j/4AAQSk...UKn0D//2Q==",
+        "name1": "/9j/4AXc5f...8KKKroB//9k="
+      },
+      "cates": {
+        "name2": "2",
+        "name1": "1"
+      }
+    }
+  ],
+  "jsonrpc": "2.0",
+  "method": "Compare",
+  "id": 12345
+}),  
+      complete: function( res ) {  
+        console.log(res);
+      }  
+    });
   },
 
 
