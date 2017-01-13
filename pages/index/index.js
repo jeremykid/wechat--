60d38254c81f220dfe938e194df46b9acd4f0a4d
weijiesun <weijie2@ualberta.ca>
2017-01-12 18:18:45 -0700

    Add the UI for two picture compare

--- pages/index/index.js
+++ pages/index/index.js
@@ -44,20 +44,41 @@ Page({
           tempFilePaths: res.tempFilePaths  
         })  
        var tempFilePath = res.tempFilePaths[0];  
-       new AV.File('image', {  
-          blob: {  
-            uri: tempFilePath,  
-          },  
-        }).save().then(  
-          file => console.log(file.url())  
-          ).catch(console.error); 
+
       }  
     })
   },
   rate: function() {
     this.setData({
         score: 80
+      });
+    var result = this.data.tempFilePaths;
+    var image = new Image();
+    image.src = result;
+    // todo change 
+    // wx.request( {  
+    //   url: "http://op.juhe.cn/onebox/weather/query",  
+    //   header: {  
+    //     "Content-Type": "application/x-www-form-urlencoded"  
+    //   },  
+    //   method: "POST",  
+    //  //data: { cityname: "上海", key: "1430ec127e097e1113259c5e1be1ba70" },  
+    //   data: Util.json2Form( { cityname: "上海", key: "1430ec127e097e1113259c5e1be1ba70" }),  
+    //   complete: function( res ) {  
+    //     this.setData( {  
+    //       toastHidden: false,  
+    //       toastText: res.data.reason,  
+    //       city_name: res.data.result.data.realtime.city_name,  
+    //       date: res.data.result.data.realtime.date,  
+    //       info: res.data.result.data.realtime.weather.info,  
+    //     });  
+    //     if( res == null || res.data == null ) {  
+    //       console.error( '网络请求失败' );  
+    //       return;  
+    //     }  
+    //   }  
+    // });
+  },
 
-      })
-  }
+
 })
